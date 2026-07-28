158f58e1cfea5162680e533e537534be88cfc273
Dhayyy7 <yogatamadhani93@gmail.com>
2026-07-28 12:32:58 +0700

    Fix : seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,15 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Role;
-use App\Models\Room;
-use App\Models\Facility;
-use App\Models\ExtraFacility;
-use App\Models\Booking;
 use App\Models\Setting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,7 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Seed default roles
+        // 1. Seed Default Roles
         $roles = [
             [
                 'name' => 'Super Admin',
@@ -54,7 +49,7 @@ class DatabaseSeeder extends Seeder
             );
         }
 
-        // Seed Admin user
+        // 2. Seed Super Admin User
         User::updateOrCreate(
             ['username' => 'admin'],
             [
@@ -66,124 +61,16 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // Seed default settings
+        // 3. Seed Default Homestay Settings
         Setting::updateOrCreate(
             ['id' => 1],
             [
                 'logo' => null,
                 'homestay_name' => 'Faradisa HomeStay',
                 'wa_number' => '081234567890',
+                'address' => 'Komplek Green Tasbih Jalan Tawaf, IX Kel No.2, RT.06/RW.04, Loktabat Selatan, Kec. Banjarbaru Selatan, Kota Banjar Baru, Kalimantan Selatan 70714',
+                'gmap_link' => 'https://maps.app.goo.gl/EJES9oxcGavzTXEs7',
                 'media_assets' => [],
-            ]
-        );
-
-        // Seed master facilities
-        $facilities = [
-            ['name' => 'AC (Air Conditioner)', 'icon' => 'fa-snowflake', 'description' => 'Pendingin ruangan dingin & sejuk.'],
-            ['name' => 'Wi-Fi Gratis 100Mbps', 'icon' => 'fa-wifi', 'description' => 'Koneksi internet cepat 24 jam.'],
-            ['name' => 'Smart TV 43 Inch', 'icon' => 'fa-tv', 'description' => 'Siaran TV kabel & Netflix.'],
-            ['name' => 'Pemanas Air (Water Heater)', 'icon' => 'fa-shower', 'description' => 'Mandi air hangat nyaman.'],
-            ['name' => 'Dapur Mini & Kulkas', 'icon' => 'fa-utensils', 'description' => 'Fasilitas memasak ringan & pendingin minuman.'],
-            ['name' => 'Sarapan Gratis', 'icon' => 'fa-mug-hot', 'description' => 'Menu sarapan lezat setiap pagi.'],
-        ];
-
-        $facilityModels = [];
-        foreach ($facilities as $f) {
-            $facilityModels[$f['name']] = Facility::updateOrCreate(
-                ['name' => $f['name']],
-                $f
-            );
-        }
-
-        // Seed Extra Facilities
-        $extraFacilities = [
-            ['name' => 'Extra Bed / Kasur Tambahan', 'price' => 75000, 'description' => 'Kasur lipat empuk + bantal & selimut.'],
-            ['name' => 'Handuk & Alat Mandi Ekstra', 'price' => 25000, 'description' => 'Set handuk bersih dan perlengkapan mandi.'],
-            ['name' => 'Sarapan Pagi Tambahan', 'price' => 35000, 'description' => 'Porsi sarapan prasmanan per orang.'],
-            ['name' => 'BBQ Grill Set & Arang', 'price' => 100000, 'description' => 'Peralatan memanggang Daging/Ikan + arang 2kg.'],
-            ['name' => 'Layanan Antar Jemput Bandara/Stasiun', 'price' => 150000, 'description' => 'Mobil AC bersih penjemputan tamu.'],
-        ];
-
-        foreach ($extraFacilities as $ef) {
-            ExtraFacility::updateOrCreate(
-                ['name' => $ef['name']],
-                $ef
-            );
-        }
-
-        // Seed sample rooms
-        $room1 = Room::updateOrCreate(
-            ['code' => 'P1V1'],
-            [
-                'code' => 'P1V1',
-                'name' => 'Paradisa 1 Vila 1',
-                'price' => 550000,
-                'discount' => 10.00, // 10% discount
-                'images' => [],
-            ]
-        );
-        $room1->facilities()->sync([
-            $facilityModels['AC (Air Conditioner)']->id,
-            $facilityModels['Wi-Fi Gratis 100Mbps']->id,
-            $facilityModels['Smart TV 43 Inch']->id,
-            $facilityModels['Pemanas Air (Water Heater)']->id,
-        ]);
-
-        $room2 = Room::updateOrCreate(
-            ['code' => 'P2V1'],
-            [
-                'code' => 'P2V1',
-                'name' => 'Paradisa 2 Vila 1',
-                'price' => 850000,
-                'discount' => 15.00, // 15% discount
-                'images' => [],
-            ]
-        );
-        $room2->facilities()->sync([
-            $facilityModels['AC (Air Conditioner)']->id,
-            $facilityModels['Wi-Fi Gratis 100Mbps']->id,
-            $facilityModels['Smart TV 43 Inch']->id,
-            $facilityModels['Pemanas Air (Water Heater)']->id,
-            $facilityModels['Dapur Mini & Kulkas']->id,
-            $facilityModels['Sarapan Gratis']->id,
-        ]);
-
-        $room3 = Room::updateOrCreate(
-            ['code' => 'P2V2'],
-            [
-                'code' => 'P2V2',
-                'name' => 'Paradisa 2 Vila 2',
-                'price' => 350000,
-                'discount' => null, // no discount
-                'images' => [],
-            ]
-        );
-        $room3->facilities()->sync([
-            $facilityModels['AC (Air Conditioner)']->id,
-            $facilityModels['Wi-Fi Gratis 100Mbps']->id,
-        ]);
-
-        // Seed sample booking
-        Booking::updateOrCreate(
-            ['booking_code' => 'P1V120260729'],
-            [
-                'room_id' => $room1->id,
-                'booking_code' => 'P1V120260729',
-                'customer_name' => 'Budi Santoso',
-                'customer_address' => 'Jl. Merdeka No. 45, Jakarta Selatan',
-                'customer_phone' => '081234567890',
-                'customer_sosmed' => '@budisantoso',
-                'check_in_date' => '2026-07-29',
-                'check_out_date' => '2026-07-31',
-                'total_nights' => 2,
-                'room_price' => 550000,
-                'discount' => 10.00,
-                'total_price' => 1065000, // (495,000 * 2) + 75,000 (Extra Bed)
-                'status' => 1, // Pending (Menunggu Bayar WA 2 jam)
-                'expired_at' => Carbon::now()->addHours(2),
-                'extra_facilities' => [
-                    ['name' => 'Extra Bed / Kasur Tambahan', 'price' => 75000]
-                ],
             ]
         );
     }
